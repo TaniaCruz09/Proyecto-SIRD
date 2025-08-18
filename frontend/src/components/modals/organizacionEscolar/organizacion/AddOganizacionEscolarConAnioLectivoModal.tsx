8cc171145a8fb96250f03ce00e6cb86d269e070d
TaniaCruz09 <cruztania0909@gmail.com>
2025-08-17 22:56:45 -0600

    feat: add new components to organzacionEscolar

---
+++ frontend/src/components/modals/organizacionEscolar/organizacion/AddOganizacionEscolarConAnioLectivoModal.tsx
@@ -0,0 +1,37 @@
+import BtnOpenAddModal from '@/components/Buttons/btnOpenAddModal';
+import React, { useState } from 'react'
+import ModalBase from '../../ModalBase';
+import OrganizacionEscolarForm from '@/components/forms/organizacionEscolarForms/OrganizacionEscolarForm';
+import { OrganizacionEscolarConAnioLectivoForm } from '@/components/forms/organizacionEscolarForms/OrganizacionEscolarConAñoLectivoForm';
+
+interface AddOganizacionEscolarConAnioLectivoModalProp {
+    fetchOrganizacionPorAnioLectivo: () => Promise<void>
+    anioLectivo: string;
+    idAnioLectivo: number
+}
+
+export default function AddOganizacionEscolarConAnioLectivoModal({ fetchOrganizacionPorAnioLectivo, anioLectivo, idAnioLectivo }: AddOganizacionEscolarConAnioLectivoModalProp) {
+    const [showModal, setShowModal] = useState<boolean>(false);
+    return (
+        <div>
+            <BtnOpenAddModal text={"Agregar nueva organizacion"} onClick={() => setShowModal(true)} />
+            {
+                showModal && (
+                    <ModalBase
+                        onshowModal={showModal}
+                        onCloseModal={() => setShowModal(false)}
+                        content={
+                            <OrganizacionEscolarConAnioLectivoForm
+                                anioLectivo={anioLectivo}
+                                idAnioLectivo={idAnioLectivo}
+                                onSuccess={() => {
+                                    fetchOrganizacionPorAnioLectivo()
+                                    setShowModal(false)
+                                }} />
+                        }
+                    />
+                )
+            }
+        </div>
+    )
+}
