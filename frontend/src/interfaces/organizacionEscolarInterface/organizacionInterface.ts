5adb8db46200179cab41229f3f5e20e6d02b619c
TaniaCruz09 <cruztania0909@gmail.com>
2025-08-17 22:42:57 -0600

    feat: remove grupo, docenteGuia, docentes, asignaturas fields and add turno, modalidad fields in organizacionInterface

--- frontend/src/interfaces/organizacionEscolarInterface/organizacionInterface.ts
+++ frontend/src/interfaces/organizacionEscolarInterface/organizacionInterface.ts
@@ -1,24 +1,21 @@
 import { AnioLectivo } from "../catalogoInterface/AnioLectivo";
 import { Asignatura } from "../catalogoInterface/AsignaturaInterface";
 import { Corte } from "../catalogoInterface/corteEvaluativoInterface";
+import { Modalidad } from "../catalogoInterface/ModalidadInterface";
+import { Turno } from "../catalogoInterface/TurnoInterface";
 import { Docente } from "../DocenteInterface";
 import { GrupoEscolar } from "./grupoInterface";
 
 export interface OrganizacionEscolar {
     id: number,
     anio_lectivo: AnioLectivo,
-    grupo: GrupoEscolar,
-    docenteGuia: Docente,
-    docentes: Docente[],
-    asignaturas: Asignatura[],
-    cortes: Corte[]
+    turno: Turno,
+    corte: Corte
+    grupo?: GrupoEscolar,
 }
 
 export interface OrganizacionEscolarPayload {
-    anio_lectivo: AnioLectivo,
-    grupo: GrupoEscolar,
-    docenteGuia: Docente,
-    docentes: Docente[],
-    asignaturas: Asignatura[],
-    cortes: Corte[]
+    anio_lectivo: { id: number };
+    turno: { id: number };
+    corte: { id: number };
 }
