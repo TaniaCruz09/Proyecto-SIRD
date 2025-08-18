83bc22d634bcf7c5832b30ba9d3944a9ff85c63d
TaniaCruz09 <cruztania0909@gmail.com>
2025-08-17 21:55:36 -0600

    refactor: modifying the login desing

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -51,53 +51,71 @@ export default function LoginPage() {
     }
   }
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <form
-        onSubmit={handleSubmit}
-        className="bg-white p-8 rounded-2xl shadow-xl w-full max-w-sm space-y-6"
-      >
-        <img src="/logo_actual_instituto_ruben_dario.png" alt="Mi logo" className='w-20 h-auto mx-auto' />
-        <h1 className="text-2xl font-bold text-center text-blue-600">Inicia Sesión</h1>
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-purple-50 via-gray-50 to-purple-100 px-4">
+      <div className="w-full max-w-sm">
 
-        {error && <p className="text-red-500 text-center">{error}</p>} {/* Mensaje de error */}
+        {/* <h1 className="text-xl text-center font-bold text-black">
+          Sistema Escolar
+        </h1>
+        <h1 className="text-md text-center text-black mb-5">
+          Instituto Rubén Darío
+        </h1> */}
 
-        {sessionExpired && (
-          <p className="text-yellow-600 bg-yellow-100 border border-yellow-300 p-3 rounded text-sm text-center">
-            Tu sesión ha caducado. Por favor, inicia sesión nuevamente.
-          </p>
-        )}
-
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Correo"
-          className="w-full p-3 border rounded-xl border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-400 text-black"
-          required
-        />
-
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Contraseña"
-          className="w-full p-3 border rounded-xl border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-400 text-black"
-          required
-        />
-        <p className="text-center text-sm text-gray-600">
-          <a href="/crear la pagina" className="text-blue-600 hover:underline">
-            ¿Olvidó su contraseña?
-          </a>
-        </p>
-
-        <button
-          type="submit"
-          className="w-full bg-blue-500 text-white py-3 rounded-xl hover:bg-blue-600 transition"
-          disabled={loading}
+        <form
+          onSubmit={handleSubmit}
+          className="bg-white/90 backdrop-blur-sm p-8 rounded-3xl shadow-2xl w-full border border-purple-200"
         >
-          {loading ? 'Cargando...' : 'Iniciar sesión'}
-        </button>
-      </form>
+          <img
+            src="/logo_actual_instituto_ruben_dario.png"
+            alt="Mi logo"
+            className="w-20 h-auto mx-auto drop-shadow-xl mb-4"
+          />
+          <div className="space-y-5">
+            <h1 className="text-2xl font-bold text-center text-blue-900/90">Iniciar Sesión</h1>
+
+            {error && <p className="text-red-500 text-center">{error}</p>}
+
+            {sessionExpired && (
+              <p className="text-yellow-600 bg-yellow-100 border border-yellow-300 p-3 rounded text-sm text-center">
+                Tu sesión ha caducado. Por favor, inicia sesión nuevamente.
+              </p>
+            )}
+
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Correo"
+              className="w-full p-3 border rounded-xl border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-400 text-black"
+              required
+            />
+
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Contraseña"
+              className="w-full p-3 border rounded-xl border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-400 text-black"
+              required
+            />
+
+            <p className="text-center text-sm text-gray-600">
+              <a href="/crear la pagina" className="text-blue-600 hover:underline">
+                ¿Olvidó su contraseña?
+              </a>
+            </p>
+
+            <button
+              type="submit"
+              className="w-full bg-gradient-to-r from-sky-500 to-blue-600 text-white py-3 rounded-xl hover:from-sky-600 hover:to-blue-700 transition shadow-lg font-semibold"
+              disabled={loading}
+            >
+              {loading ? 'Cargando...' : 'Iniciar sesión'}
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
+
   )
 }
