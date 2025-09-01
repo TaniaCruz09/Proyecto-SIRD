9a5528ae89034ab08467026cd6cdef2b8a110a7e
TaniaCruz09 <cruztania0909@gmail.com>
2025-08-31 23:34:24 -0600

    cree nuevos modulos para la asignacion de estudiantes a los grupos con asignaturas

---
+++ Backend/src/module/organizacionEscolar/dtos/grupos-asignatura-con-estudiantes.dto.ts
@@ -0,0 +1,22 @@
+import { IsNotEmpty, IsNumber, IsObject, IsOptional, ValidateNested } from "class-validator";
+import { Type } from "class-transformer";
+import { StudentEntity } from "src/module/createEstudents";
+import { GrupoAsignaturaDocente } from "../entities/GrupoAsignaturaDocente.entity";
+
+export class AsignarEstudianteAGrupoDto {
+  @IsOptional()
+  @IsNumber()
+  readonly id?: number;
+
+  @IsNotEmpty()
+  @IsObject()
+  estudiante: StudentEntity;
+
+  @IsNotEmpty()
+  @IsObject()
+  grupoAsignaturaDocente: GrupoAsignaturaDocente;
+
+  @IsOptional()
+  @IsNumber()
+  user_create_id?: number;
+}
---
+++ Backend/src/module/organizacionEscolar/entities/grupo-asignatura-con-estudiantes.entity.ts
@@ -0,0 +1,79 @@
+import { StudentEntity } from 'src/module/createEstudents';
+import { User } from '../../auth/entities';
+import * as moment from 'moment-timezone';
+import { Grupos } from './grupos.entity';
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { GrupoAsignaturaDocente } from './GrupoAsignaturaDocente.entity';
+
+@Entity({ name: "grupo_asignatura_con_estudiantes", schema: 'organizacion_escolar' })
+export class GrupoAsignaturaConEstudiantes {
+  @PrimaryGeneratedColumn({
+    name: 'id',
+    type: 'int2',
+  })
+  id: number;
+
+  @ManyToOne(() => GrupoAsignaturaDocente, grupo => grupo.gruposConEstudiantes, {
+    onDelete: "CASCADE",
+  })
+  grupoAsignaturaDocente: GrupoAsignaturaDocente;
+
+  @ManyToOne(() => StudentEntity, estudiante => estudiante.grupoAsignaturaConEstudiantes, {
+    onDelete: "CASCADE",
+  })
+  estudiante: StudentEntity;
+
+  //ID del usuario que creó el registro
+  @Column({ name: 'user_create_id', type: 'int4', nullable: true }) // Nuevo campo
+  user_create_id: number;
+
+  // Fecha y hora en que se creó el registro
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP', // Guarda la fecha en UTC por defecto
+    transformer: {
+      to: (value: Date) => value, // Guarda la fecha tal como es
+      from: (value: Date) =>
+        moment(value).tz('America/Managua').format('YYYY-MM-DD hh:mm A'), // Formatea a hora de Nicaragua
+    },
+  })
+  created_at: Date;
+
+  // Fecha y hora de la última actualización del registro
+  @UpdateDateColumn({
+    name: 'update_at',
+    type: 'timestamp',
+    onUpdate: 'CURRENT_TIMESTAMP',
+    transformer: {
+      to: (value: Date) => value,
+      from: (value: Date) =>
+        moment(value).tz('America/Managua').format('YYYY-MM-DD hh:mm A'),
+    },
+  })
+  update_at: Date;
+
+  // ID del usuario que realizó la última actualización del registro
+  @Column({ name: 'user_update_id', type: 'int4', nullable: true })
+  user_update_id: number;
+
+  // Fecha y hora en que el registro fue eliminado
+  @DeleteDateColumn({ name: 'deleted_at', type: 'timestamp', nullable: true })
+  deleted_at: Date;
+
+  // ID del usuario que elimino el registro
+  @Column({ name: 'deleted_at_id', type: 'int4', nullable: true })
+  deleted_at_id: number;
+
+  @ManyToOne(() => User, (user) => user.id)
+  @JoinColumn({ name: 'user_create_id' }) // Se enlaza con el usuario que creó el registro
+  user_create: User;
+
+  @ManyToOne(() => User, (user) => user.id)
+  @JoinColumn({ name: 'user_update_id' }) // Se enlaza a la columna 'user_update_id'
+  user_update: User;
+
+  @ManyToOne(() => User, (user) => user.id)
+  @JoinColumn({ name: 'deleted_at_id' }) // Se enlaza con el usuario que eliminó el registro
+  user_delete: User;
+}
