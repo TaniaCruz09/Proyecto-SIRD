df5892687b9c3fb236947a8ef4631fb83c5e039f
TaniaCruz09 <cruztania0909@gmail.com>
2025-09-08 13:27:38 -0600

    feat: agregando el tipado de nestEspressApplication y hacer la carpeta de uploads de forma publica

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setGlobalPrefix('api/v1')
   app.enableCors();
   app.useGlobalPipes(
@@ -17,15 +19,20 @@ async function bootstrap() {
     }),
   );
 
+  //Servir carpeta uploads de forma pública
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads',
+  });
+
   const config = new DocumentBuilder()
-  .setTitle('sird documentation')
-  .setDescription('The sird API description')
-  .setVersion('1.0')
-  .addBearerAuth()
-  .addTag('sird')
-  .build();
-const documentFactory = () => SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('docs', app, documentFactory);
+    .setTitle('sird documentation')
+    .setDescription('The sird API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addTag('sird')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
 
   await app.listen(process.env.PORT || 3000);
 }
