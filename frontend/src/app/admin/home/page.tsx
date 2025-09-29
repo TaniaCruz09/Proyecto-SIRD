ba8af531b3dcaa9bccc8fddc6e2516a88287c291
TaniaCruz09 <cruztania0909@gmail.com>
2025-09-28 18:06:18 -0600

    style: mejorando diseño de las vistas de home separando el header

--- frontend/src/app/admin/home/page.tsx
+++ frontend/src/app/admin/home/page.tsx
@@ -10,33 +10,33 @@ import Header from '@/components/Header'
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-purple-100/30">
       <Header title='Sistema de Calificaciones SIRD' subTitle='Panel de Administración' />
       <main className="max-w-7xl mx-auto px-6">
         <div className="m-5">
-          <h2 className="text-2xl font-bold text-foreground mb-3">Bienvenido Administrador</h2>
-          <p className="text-lg text-muted-foreground">Administra fácilmente las funciones del sistema desde aquí</p>
+          <h2 className="text-2xl font-bold text-foreground mb-1">Bienvenido Administrador</h2>
+          <p className=" text-muted-foreground">Administra fácilmente las funciones del sistema desde aquí</p>
         </div>
 
-        <div className="flex items-center bg-gray-100 rounded-xl pl-3 font-semibold text-black">
+        <div className="flex items-center bg-purple-100/30 rounded-xl pl-3 font-semibold text-black">
           <FaArrowUpRightFromSquare />
           <p className="pl-2">Accesos directos</p>
         </div>
 
         {/* Tarjetas de acceso directo */}
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-5 place-items-center bg-white border rounded-xl p-5 mb-5">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-5 place-items-center mb-5">
 
           {/* Card 1: Usuarios y Roles */}
           <Link href={"/auth/users"}>
             <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.98 }} className="w-70">
               <Card className="bg-gradient-to-br from-blue-200 to-blue-100 hover:from-blue-100 hover:to-blue-200 rounded-2xl shadow-lg flex flex-col items-center justify-center transition-all hover:shadow-2xl cursor-pointer">
-                <CardHeader className="flex flex-col items-center">
-                  <FaUserShield className="text-black text-3xl" />
-                </CardHeader>
                 <CardContent className="text-center">
-                  <div className="text-xl font-bold text-slate-800">Usuarios y Roles</div>
+                  <CardHeader className="flex flex-col items-center">
+                    <FaUserShield className="text-blue-900 text-2xl" />
+                  </CardHeader>
+                  <div className="text-lg font-bold text-slate-800">Gestiona Usuarios</div>
                   <p className="text-sm text-slate-600">
-                    Gestiona los usuarios del sistema y sus permisos
+                    Agregar, editar o eliminar
                   </p>
                 </CardContent>
               </Card>
@@ -48,13 +48,13 @@ export default function HomePage() {
           <Link href={"/calificaciones"}>
             <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.98 }} className="w-70">
               <Card className="bg-gradient-to-br from-emerald-200 to-emerald-100 hover:from-emerald-100 hover:to-emerald-200 rounded-2xl shadow-lg flex flex-col items-center justify-center transition-all hover:shadow-2xl cursor-pointer">
-                <CardHeader className="flex flex-col items-center">
-                  <FaBook className="text-black text-3xl" />
-                </CardHeader>
                 <CardContent className="text-center">
-                  <div className="text-xl font-bold text-slate-800">Calificaciones</div>
+                  <CardHeader className="flex flex-col items-center">
+                    <FaBook className="text-emerald-900 text-2xl" />
+                  </CardHeader>
+                  <div className="text-lg font-bold text-slate-800">Calificaciones</div>
                   <p className="text-sm text-slate-600">
-                    Administra las calificaciones de los grados y más
+                    Revisa calificaciones de los grados
                   </p>
                 </CardContent>
               </Card>
@@ -65,13 +65,13 @@ export default function HomePage() {
           <Link href={"/registerDocente"}>
             <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.98 }} className="w-70">
               <Card className="bg-gradient-to-br from-purple-200 to-purple-100 hover:from-purple-100 hover:to-purple-200 rounded-2xl shadow-lg flex flex-col items-center justify-center transition-all hover:shadow-2xl cursor-pointer">
-                <CardHeader className="flex flex-col items-center">
-                  <FaUsers className="text-black text-3xl" />
-                </CardHeader>
                 <CardContent className="text-center">
-                  <div className="text-xl font-bold text-slate-800">Registro de Docentes</div>
+                  <CardHeader className="flex flex-col items-center">
+                    <FaUsers className="text-purple-800 text-2xl" />
+                  </CardHeader>
+                  <div className="text-lg font-bold text-slate-800">Gestion Docentes</div>
                   <p className="text-sm text-slate-600">
-                    Agrega o edita los docentes del sistema
+                    Agregar, editar o eliminar
                   </p>
                 </CardContent>
               </Card>
@@ -82,20 +82,20 @@ export default function HomePage() {
           <Link href={"/registerEstudent"}>
             <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.98 }} className="w-70">
               <Card className="bg-gradient-to-br from-amber-200 to-amber-100 hover:from-amber-100 hover:to-amber-200 rounded-2xl shadow-lg flex flex-col items-center justify-center transition-all hover:shadow-2xl cursor-pointer">
-                <CardHeader className="flex flex-col items-center">
-                  <FaGraduationCap className="text-black text-3xl" />
-                </CardHeader>
                 <CardContent className="text-center">
-                  <div className="text-xl font-bold text-slate-800">Estudiantes</div>
+                  <CardHeader className="flex flex-col items-center">
+                    <FaGraduationCap className="text-yellow-700 text-2xl" />
+                  </CardHeader>
+                  <div className="text-lg font-bold text-slate-800">Gestion Estudiantes</div>
                   <p className="text-sm text-slate-600">
-                    Gestiona los estudiantes registrados
+                    Agregar, editar o eliminar
                   </p>
                 </CardContent>
               </Card>
             </motion.div>
           </Link>
         </div>
-        <EstadisticaAdmin />
+        {/* <EstadisticaAdmin /> */}
         <div className="bg-card rounded-lg border border-border p-6 ">
           <AcademicYearsDashboard />
         </div>
