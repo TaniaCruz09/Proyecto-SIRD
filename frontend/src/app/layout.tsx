21691ffadf2b1165091dd5b8a3f9d27c1359b8f8
mantopage <mantopage@gmail.com>
2025-09-29 08:40:19 -0600

    agregando TanStack Query al json

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,7 +3,9 @@ import { Geist, Geist_Mono, } from "next/font/google";
 import "./globals.css";
 import NavbarAdmin from "@/components/navbarAdmin";
 import { ToastProvider } from "@/hooks/use-toast";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
 
+const queryClient = new QueryClient();
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -38,6 +40,7 @@ html {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex h-screen bg-white`}
       >
+        <QueryClientProvider client={queryClient}>
         <NavbarAdmin />
         <main className="w-screen text-center bg-gray-100 overflow-auto">
           <ToastProvider>
@@ -45,7 +48,7 @@ html {
             {children}
           </ToastProvider>
         </main>
-
+        </QueryClientProvider>
       </body>
     </html>
   );
