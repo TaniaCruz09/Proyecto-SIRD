c27f96198c1bfa29953a478c4ea8024b1375b77d
Mitch-Luna <lunitamitch02@gmail.com>
2026-03-02 18:45:14 -0600

    corrigiendo algo

--- Backend/src/module/organizacionEscolar/services/grupos.service.ts
+++ Backend/src/module/organizacionEscolar/services/grupos.service.ts
@@ -1,10 +1,14 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Grupos } from '../entities/grupos.entity';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
 import { Utilities } from '../../../common/helpers/utilities';
 import { CreateGrupoDto } from '../../organizacionEscolar/dtos/grupos.dto';
 import { UpdateGrupoDto } from '../../organizacionEscolar/dtos/Update-grupo.dto';
+import { GrupoAsignaturaDocente } from '../entities/GrupoAsignaturaDocente.entity';
+import { GrupoAsignaturaConEstudiantes } from '../entities/grupo-asignatura-con-estudiantes.entity';
+import { EsquelaHeadEntity } from 'src/module/calificaciones/esquela_head/entities/squela_head.entity';
+import { EsquelaRow } from 'src/module/calificaciones/esquelas_rows/esquelas_rows.entity';
 
 
 @Injectable()
@@ -107,7 +111,50 @@ export class GruposService {
             const grupos = await this.grupoRepository.findOne({
                 where: { id: id }
             });
-            return await this.grupoRepository.remove(grupos)
+
+            if (!grupos) {
+                throw new NotFoundException(`Grupo con ID ${id} no encontrado`);
+            }
+
+            await this.grupoRepository.manager.transaction(async (manager) => {
+                const esquelaHeads = await manager.getRepository(EsquelaHeadEntity).find({
+                    select: ['id'],
+                    where: { grupo_asignatura: { id } },
+                });
+
+                const esquelaHeadIds = esquelaHeads.map((item) => item.id);
+
+                if (esquelaHeadIds.length > 0) {
+                    await manager.getRepository(EsquelaRow).delete({
+                        esquelaHead: { id: In(esquelaHeadIds) },
+                    });
+                }
+
+                await manager.getRepository(EsquelaHeadEntity).delete({
+                    grupo_asignatura: { id },
+                });
+
+                await manager
+                    .createQueryBuilder()
+                    .delete()
+                    .from(GrupoAsignaturaConEstudiantes)
+                    .where(`"grupoAsignaturaDocenteId" IN (
+                        SELECT gad.id
+                        FROM "organizacion_escolar"."grupo_asignatura_docente" gad
+                        WHERE gad."grupo_id" = :grupoId
+                    )`, { grupoId: id })
+                    .execute();
+
+                await manager
+                    .getRepository(GrupoAsignaturaDocente)
+                    .delete({ grupo: { id } });
+
+                await manager
+                    .getRepository(Grupos)
+                    .remove(grupos);
+            });
+
+            return grupos;
         } catch (error) {
             Utilities.catchError(error)
         }
